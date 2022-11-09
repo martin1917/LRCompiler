66f24c612129ba4c1de07f0e895e9874d4ee6bf7
martin1917 <martin.levin02@mail.ru>
2022-11-09 17:31:54 +0300

    node for tree

---
+++ LRv2/AST/TreeNode.cs
@@ -0,0 +1,26 @@
+﻿namespace LRv2.AST;
+
+public class TreeNode
+{
+    public string Value { get; }
+
+    public List<TreeNode> Childs { get; }
+
+    public TreeNode(string value) : this(value, new List<TreeNode>()) { }
+
+    public TreeNode(string value, List<TreeNode> childs)
+    {
+        Value = value;
+        Childs = childs;
+    }
+
+    public void Add(TreeNode node)
+    {
+        Childs.Add(node);
+    }
+
+    public override string? ToString()
+    {
+        return Value;
+    }
+}
